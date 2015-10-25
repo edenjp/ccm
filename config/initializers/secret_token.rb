2a459f464819d196eeadcf10c2a25a5da4764946
Éden Pereira <eden.pereira@locaweb.com.br>
2015-10-25 16:58:01 -0200

    Inicio do projeto CMM

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ccm::Application.config.secret_key_base = 'b5f9540955dde35e5f7738aa08edfd1b52ec9775cefae2cbbd07f4e863a79a62f287a380198817fc63e74220c1071b63bb51d745eb9a3af376fdda6030f410c2'
