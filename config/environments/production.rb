30157925e54b1746ece5d5115c9bcaacfaf6ef4f
eden.pereira <eden.pereira@locaweb.com.br>
2015-11-22 22:18:36 -0200

    mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,17 +83,13 @@ Ccm::Application.configure do
   }
 
   config.action_mailer.smtp_settings = {
-    :port
-    => ENV['MAILGUN_SMTP_PORT'],
-    :address
-    => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name
-    => ENV['MAILGUN_SMTP_LOGIN'],
-    :password
-    => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain
-    => 'ccm2.herokuapp.com',
+    :port    => ENV['MAILGUN_SMTP_PORT'],
+    :address    => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name    => ENV['MAILGUN_SMTP_LOGIN'],
+    :password    => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain    => 'ccm2.herokuapp.com',
     :authentication => :plain,
   }
+
   config.action_mailer.delivery_method = :smtp
 end
